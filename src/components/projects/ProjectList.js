5f4c6a28ec862ddd2fb8861d510913cfd96981a0
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-06-05 14:32:23 -0700

    modular the projectsummary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
-
+import ProjectSummary from './ProjectSummary'
 const ProjectList =() =>{
     return(
         <div className='project-list section'>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
 
-            <div className= 'card z-depth-0 project-summary'>
-                <div className='card-content grety-text text-darken-3'>
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Fluffmeiste</p>
-                    <p className='grey-text'>12th June, 3am</p>           
-                </div>
-            </div>
-
-            <div className= 'card z-depth-0 project-summary'>
-                <div className='card-content grety-text text-darken-3'>
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Fluffmeiste</p>
-                    <p className='grey-text'>12th June, 3am</p>           
-                </div>
-            </div>
-
-            <div className= 'card z-depth-0 project-summary'>
-                <div className='card-content grety-text text-darken-3'>
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Fluffmeiste</p>
-                    <p className='grey-text'>12th June, 3am</p>           
-                </div>
-            </div>
-        
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React  from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className= 'card z-depth-0 project-summary'>
+            <div className='card-content grety-text text-darken-3'>
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Fluffmeister</p>
+                <p className='grey-text'>12th June, 3am</p>           
+            </div>
+         </div>
+    )
+}
+
+export default ProjectSummary;
