99ad556c02b96cae3f9b330eba9c58dde950024b
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-06-05 14:27:39 -0700

    router

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,25 @@
+import React ,{Component} from 'react'
+import Notifications from './Notifications'
+import ProjectList from '../projects/ProjectList'
+
+
+
+class Dashboard extends Component {
+    render(){
+        return(
+         <div className='dashboard container'>
+            <div clasName='row'>
+                <div className='col s12 m6'>
+                    <ProjectList />
+                </div>
+
+                <div className='col s12 m5 offset-m1'></div>
+                    <Notifications />
+            </div>
+         </div>   
+        )
+    }
+}
+
+
+export default Dashboard;
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const ProjectList =() =>{
+    return(
+        <div className='project-list section'>
+
+            <div className= 'card z-depth-0 project-summary'>
+                <div className='card-content grety-text text-darken-3'>
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Fluffmeiste</p>
+                    <p className='grey-text'>12th June, 3am</p>           
+                </div>
+            </div>
+
+            <div className= 'card z-depth-0 project-summary'>
+                <div className='card-content grety-text text-darken-3'>
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Fluffmeiste</p>
+                    <p className='grey-text'>12th June, 3am</p>           
+                </div>
+            </div>
+
+            <div className= 'card z-depth-0 project-summary'>
+                <div className='card-content grety-text text-darken-3'>
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the Fluffmeiste</p>
+                    <p className='grey-text'>12th June, 3am</p>           
+                </div>
+            </div>
+        
+        </div>
+    )
+}
+
+export default ProjectList;
