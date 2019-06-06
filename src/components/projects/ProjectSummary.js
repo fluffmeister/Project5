636044fda32bbf0bd873fe94353db8564b7a0778
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-06-06 13:00:02 -0700

    maping data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React  from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className= 'card z-depth-0 project-summary'>
             <div className='card-content grety-text text-darken-3'>
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by the Fluffmeister</p>
                 <p className='grey-text'>12th June, 3am</p>           
             </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState={}
+const initState={
+    projects:[
+        {id:'1',title:'testing 1',content:'red blue yellow green purple orange'},
+        {id:'2',title:'testing 2',content:'red blue yellow green purple orange'},
+        {id:'3',title:'testing 3',content:'red blue yellow green purple orange'},
+    ]
+
+}
 
 const projectReducer =(state=initState, action) =>{
     return state
